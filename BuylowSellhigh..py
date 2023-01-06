2c58f1b1092d7cf48ad2a9f4716765bedb1038a3
tvenk <121521637+tvenk@users.noreply.github.com>
2023-01-06 10:15:02 -0500

    Add files via upload

---
+++ BuylowSellhigh..py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import requests
+
+ticker = str(input("Type in all caps the ticker symbol you want to check: "))
+
+url = "https://www.macroaxis.com/valuation/{0}".format(ticker) 
+
+result = requests.get(url)
+doc = BeautifulSoup(result.content, "html.parser")
+
+divs = doc.find_all("div")
+price = ""
+for div in divs:
+    if (str(div).startswith('<div style="margin-bottom:10px"')):
+        div_text = str(div)
+        index = div_text.index("$")
+        index2 = div_text.find("per share")
+        price = div_text[index:index2]
+        break
+price = price.rstrip()
+realvalue = price.lstrip("$")
+print("{0} Real value: $".format(ticker),realvalue) 
+
+
+url2 ="https://finance.yahoo.com/quote/{0}/".format(ticker) 
+
+
+result = requests.get(url2)
+
+doc2 = BeautifulSoup(result.text, "html.parser")
+
+cprice = ""
+
+tags = doc2.find_all("fin-streamer")
+
+for tag in tags:
+    if tag["data-symbol"] == ticker: 
+
+         cprice = tag["value"]
+         break
+
+print("Current {0} price: $".format(ticker),cprice) 
+
+
+if float(cprice) < float(realvalue):
+    print("Buy!")
+else:
+    print("Sell!")
+
