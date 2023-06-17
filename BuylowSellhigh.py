acaa5f980a39fbb25f8e41aec7169abe90e939e2
tvenk <121521637+tvenk@users.noreply.github.com>
2023-06-16 21:41:41 -0400

    Add files via upload

--- BuylowSellhigh.py
+++ BuylowSellhigh.py
@@ -1,48 +1,61 @@
 from bs4 import BeautifulSoup
 import requests
 
-ticker = str(input("Type in all caps the ticker symbol you want to check: "))
 
-url = "https://www.macroaxis.com/valuation/{0}".format(ticker) 
+ticker =""
 
-result = requests.get(url)
-doc = BeautifulSoup(result.content, "html.parser")
-
-divs = doc.find_all("div")
-price = ""
-for div in divs:
-    if (str(div).startswith('<div class="ui large green button"')):
-        div_text = str(div)
-        price = div.text
-        break
-        
-
-realvalue = price
-print("{0} Real value: $".format(ticker),realvalue) 
+while ticker != 0 or ticker != 1:
+    ticker = input("Type in all caps the ticker symbol you want to check. Do you want to close? 1 = Yes 0 = No: ")
+    if ticker == 1:
+        exit()
 
 
-url2 ="https://finance.yahoo.com/quote/{0}/".format(ticker) 
+    url = "https://www.macroaxis.com/valuation/{0}".format(ticker) 
 
-headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'} 
-result = requests.get(url2, headers=headers)
+    result = requests.get(url)
+    doc = BeautifulSoup(result.content, "html.parser")
 
-doc2 = BeautifulSoup(result.text, "html.parser")
+    divs = doc.find_all("div")
+    price = ""
+    for div in divs:
+        if (str(div).startswith('<div class="ui large green button"')):
+            div_text = str(div)
+            price = div.text
+            break
+            
 
-cprice = ""
-
-tags = doc2.find_all("fin-streamer")
-
-for tag in tags:
-    if tag["data-symbol"] == ticker: 
-
-         cprice = tag["value"]
-         break
-
-print("Current {0} price: $".format(ticker),cprice)
+    realvalue = price
+    realvalue = realvalue.replace(',','')
+    print("{0} Real value: $".format(ticker),realvalue) 
 
 
-if float(cprice) < float(realvalue):
-    print("Buy!")
-else:
-    print("Sell!")
+    url2 ="https://finance.yahoo.com/quote/{0}/".format(ticker) 
+
+    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'} 
+    result = requests.get(url2, headers=headers)
+
+    doc2 = BeautifulSoup(result.text, "html.parser")
+
+    cprice = ""
+
+    tags = doc2.find_all("fin-streamer")
+
+    for tag in tags:
+        if tag["data-symbol"] == ticker: 
+
+             cprice = tag["value"]
+             break
+
+    print("Current {0} price: $".format(ticker),cprice)
+
+
+    if float(cprice) < float(realvalue):
+        print("Buy!")
+    else:
+        print("Sell!")
+
+    print("\n")
+
+
+
 
